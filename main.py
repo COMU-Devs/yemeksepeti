a67d76bd8d9fcb9ac1706c3ad1154c985f88773a
gokselaktas <aktas_goksel@hotmail.com>
2019-12-15 23:26:55 +0300

    satici yarisi bitti

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ import sqlite3
 from random import randint
 
 authInfo = {
-    'id': 'thmyris',
-    'password': '5',
-    'type': 'customer'
+    'id': '1',
+    'password': '2342324',
+    'type': 'restaurant'
 }
 
 
@@ -59,6 +59,7 @@ class productQWidget(QWidget):
         self.vLayout.addWidget(self.nameLabel)
         self.vLayout.addWidget(self.ingredientsLabel)
         self.vLayout.addWidget(self.categoryLabel)
+
         self.hLayout.addWidget(self.imageLabel)
         self.hLayout.addLayout(self.vLayout)
         self.hLayout.addWidget(self.button)
@@ -278,6 +279,85 @@ class orderQWidget(QListWidget):
             self.setItemWidget(item, item_widget)
 
 
+class saticiItemQWidget(QWidget):
+    def __init__(self, row, parent=None):
+        super(saticiItemQWidget, self).__init__(parent)
+        self.parent = parent
+        self.values = row
+
+        productId, name, price, r_id, category, ingredients = row
+        imageUrl = self.getProductImage(productId)        
+        
+        self.imageLabel = QLabel()
+
+        self.imgsizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Preferred)
+        self.imgsizePolicy.setHorizontalStretch(0)
+        self.imgsizePolicy.setVerticalStretch(0)
+        self.imgsizePolicy.setHeightForWidth(
+            self.imageLabel.sizePolicy().hasHeightForWidth())
+        self.pixmap = QPixmap(imageUrl[0]).scaled(
+            30, 30, QtCore.Qt.KeepAspectRatio)
+
+        self.imageLabel.setSizePolicy(self.imgsizePolicy)
+        self.imageLabel.setPixmap(self.pixmap)
+        self.nameLabel = QLabel(str(name))
+        self.ingredientsLabel = QLabel(str(ingredients))
+        self.categoryLabel = QLabel(str(category))
+        self.priceLabel = QLabel(str(price))
+        self.button = QPushButton("Guncelle")
+        self.button_2 = QPushButton("Sil")
+
+        self.vLayout = QVBoxLayout()
+        self.hLayout = QHBoxLayout()
+        self.vLayout.addWidget(self.nameLabel)
+        self.vLayout.addWidget(self.ingredientsLabel)
+        self.vLayout.addWidget(self.categoryLabel)
+        self.hLayout.addWidget(self.imageLabel)
+        self.hLayout.addLayout(self.vLayout)
+        self.hLayout.addWidget(self.button)
+        self.hLayout.addWidget(self.button_2)
+
+        self.setLayout(self.hLayout)
+     # self.button.clicked.connect(
+        #    lambda: self.parent.grandparent.sepet.addSepetItem(productId))
+    def getProductImage(self, productId):
+        global cur
+        url = 'assets/empty.png'
+        cur.execute(
+            'SELECT url FROM product_image WHERE p_id=' + str(productId))
+        result = cur.fetchall()
+        if len(result) == 1:
+            url = result[0]
+
+        return url
+
+
+class saticiQWidget(QListWidget):
+    def __init__(self, parent=None, grandparent=None):
+        QListWidget.__init__(self, parent)
+        self.listOrders()
+
+    def listOrders(self):
+        self.clear()
+        global cur , authInfo
+        restId = authInfo['id']
+        cur.execute(
+            'SELECT product.id, product.name, product.price, product.r_id, product.category, product.ingredients FROM product , restaurant WHERE restaurant.id = product.r_id and restaurant.id = '+str(restId))
+        #SELECT product.name, product.price, product.category, product.ingredients 
+        #FROM product , restaurant 
+        #WHERE restaurant.id ='1' and restaurant.id = product.r_id
+        result = cur.fetchall()
+        print(result)
+        for row in result:
+            item = QListWidgetItem(self)
+            item_widget = saticiItemQWidget(row, parent=self)
+            item.setSizeHint(item_widget.sizeHint())
+            self.addItem(item)
+            self.setItemWidget(item, item_widget)
+
+
+
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
@@ -306,7 +386,8 @@ class Ui_MainWindow(object):
         self.orders.setGeometry(QtCore.QRect(450, 260, 421, 220))
         self.orders.setObjectName('orders')
 
-        self.tabWidget.addTab(self.tab, "")  # adding tab to tabWidget
+        self.tabWidget.addTab(self.tab, "")  #adding tab to tabWidget
+
 # ---END OF TAB: ANASAYFA
 # ---INIT TAB: ADMIN
         self.tab_2 = QtWidgets.QWidget()  # tab_2 = Admin
@@ -318,11 +399,109 @@ class Ui_MainWindow(object):
         self.tab_3 = QtWidgets.QWidget()  # tab_3 = Satici
         self.tab_3.setObjectName("Satici")
 
+        self.satici_listView = saticiQWidget(self.tab_3, grandparent=self)
+        self.satici_listView.setGeometry(QtCore.QRect(50, 50, 441, 231))
+        self.satici_listView.setObjectName("listView")#yemekler
+        
+        self.satici_yemekler = QtWidgets.QLabel(self.tab_3)
+        self.satici_yemekler.setGeometry(QtCore.QRect(50, 30, 67, 17))
+        self.satici_yemekler.setObjectName("yemekler")
+
+        self.satici_listeyeEkleButton = QtWidgets.QPushButton(self.tab_3)
+        self.satici_listeyeEkleButton.setGeometry(QtCore.QRect(390, 450, 89, 25))
+        self.satici_listeyeEkleButton.setCheckable(True)
+        self.satici_listeyeEkleButton.setObjectName("listeyeEkleButton")
+        self.satici_listeyeEkleButton.clicked.connect(self.listeyeEkle)
+
+        self.satici_productNameText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_productNameText.setGeometry(QtCore.QRect(190, 290, 301, 31))
+        self.satici_productNameText.setObjectName("productNameText")
+
+        self.satici_productPriceText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_productPriceText.setGeometry(QtCore.QRect(190, 330, 301, 31))
+        self.satici_productPriceText.setObjectName("productPriceText")
+
+        self.satici_categoryText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_categoryText.setGeometry(QtCore.QRect(190, 370, 301, 31))
+        self.satici_categoryText.setObjectName("categoryText")
+
+        self.satici_ingredientsText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_ingredientsText.setGeometry(QtCore.QRect(190, 410, 301, 31))
+        self.satici_ingredientsText.setObjectName("ingredientsText")
+
+        self.satici_productName = QtWidgets.QLabel(self.tab_3)
+        self.satici_productName.setGeometry(QtCore.QRect(60, 300, 101, 16))
+        self.satici_productName.setObjectName("productName")
+
+        self.satici_productPrice = QtWidgets.QLabel(self.tab_3)
+        self.satici_productPrice.setGeometry(QtCore.QRect(60, 340, 101, 17))
+        self.satici_productPrice.setObjectName("productPrice")
+
+        self.satici_category = QtWidgets.QLabel(self.tab_3)
+        self.satici_category.setGeometry(QtCore.QRect(60, 380, 101, 17))
+        self.satici_category.setObjectName("category")
+
+        self.satici_ingredients = QtWidgets.QLabel(self.tab_3)
+        self.satici_ingredients.setGeometry(QtCore.QRect(60, 420, 101, 17))
+        self.satici_ingredients.setObjectName("ingredients")
+
+        self.satici_productImage = QtWidgets.QLabel(self.tab_3)
+        self.satici_productImage.setGeometry(QtCore.QRect(60, 460, 101, 17))
+        self.satici_productImage.setObjectName("productImage")
+
+        self.satici_imageOpenButton = QtWidgets.QPushButton(self.tab_3)
+        self.satici_imageOpenButton.setGeometry(QtCore.QRect(190, 450, 89, 25))
+        self.satici_imageOpenButton.setObjectName("imageOpenButton")
+        self.satici_imageOpenButton.clicked.connect(self.importInput)
+
+        self.satici_minPriceUpdate = QtWidgets.QLabel(self.tab_3)
+        self.satici_minPriceUpdate.setGeometry(QtCore.QRect(560, 50, 171, 31))
+        self.satici_minPriceUpdate.setObjectName("minPriceUpdate")
+        
+
+        self.satici_minPriceUpdateText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_minPriceUpdateText.setGeometry(QtCore.QRect(740, 50, 301, 31))
+        self.satici_minPriceUpdateText.setObjectName("minPriceUpdateText")
+
+        self.satici_minPriceUpdateButton = QtWidgets.QPushButton(self.tab_3)
+        self.satici_minPriceUpdateButton.setGeometry(QtCore.QRect(1050, 50, 89, 25))
+        self.satici_minPriceUpdateButton.setObjectName("minPriceUpdateButton")
+        self.satici_minPriceUpdateButton.clicked.connect(self.minFiyatGuncelleme)
+
+        self.satici_restAddressUpdate = QtWidgets.QLabel(self.tab_3)
+        self.satici_restAddressUpdate.setGeometry(QtCore.QRect(560, 100, 171, 31))
+        self.satici_restAddressUpdate.setObjectName("restAddressUpdate")
+
+        self.satici_restAddressUpdateText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_restAddressUpdateText.setGeometry(QtCore.QRect(740, 100, 301, 31))
+        self.satici_restAddressUpdateText.setObjectName("restAddressUpdateText")
+
+        self.satici_restAddressUpdateButton = QtWidgets.QPushButton(self.tab_3)
+        self.satici_restAddressUpdateButton.setGeometry(QtCore.QRect(1050, 100, 89, 25))
+        self.satici_restAddressUpdateButton.setObjectName("restAddressUpdateButton")
+        self.satici_restAddressUpdateButton.clicked.connect(self.restAddressGuncelleme)
+
+        self.satici_restNameUpdate = QtWidgets.QLabel(self.tab_3)
+        self.satici_restNameUpdate.setGeometry(QtCore.QRect(560, 150, 171, 31))
+        self.satici_restNameUpdate.setObjectName("restNameUpdate")
+
+        self.satici_restNameUpdateText = QtWidgets.QTextEdit(self.tab_3)
+        self.satici_restNameUpdateText.setGeometry(QtCore.QRect(740, 150, 301, 31))
+        self.satici_restNameUpdateText.setObjectName("restNameUpdateText")
+
+        self.satici_restNameUpdateButton = QtWidgets.QPushButton(self.tab_3)
+        self.satici_restNameUpdateButton.setGeometry(QtCore.QRect(1050, 150, 89, 25))
+        self.satici_restNameUpdateButton.setObjectName("restNameUpdateButton")
+        self.satici_restNameUpdateButton.clicked.connect(self.restIsmiGuncelleme)
+
         self.tabWidget.addTab(self.tab_3, "")  # adding tab_3 to tabWidget
 # ---END OF TAB: SATICI
 # ---INIT TAB: PROFIL
         self.tab_4 = QtWidgets.QWidget()  # tab_4 = Profil
         self.tab_4.setObjectName("Profil")
+
+
+        
 
         self.tabWidget.addTab(self.tab_4, "")  # adding tab_4 to tabWidget
 # ---END OF TAB: PROFIL
@@ -340,6 +519,8 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.importImagePath = "NULL" #goksel 1 satir
+
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -352,7 +533,97 @@ class Ui_MainWindow(object):
             self.tab_3), _translate("MainWindow", "Satici"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(
             self.tab_4), _translate("MainWindow", "Profil"))
+        
 
+        #goksel
+        self.satici_yemekler.setText(_translate("MainWindow", "Yemekler"))
+        self.satici_listeyeEkleButton.setText(_translate("MainWindow", "Listeye Ekle"))
+        self.satici_productName.setText(_translate("MainWindow", "Yemek ismi"))
+        self.satici_productPrice.setText(_translate("MainWindow", "Yemek Fiyati"))
+        self.satici_category.setText(_translate("MainWindow", "Kategori"))
+        self.satici_ingredients.setText(_translate("MainWindow", "Icindekiler"))
+        self.satici_productImage.setText(_translate("MainWindow", "Yemek Resmi"))
+        self.satici_imageOpenButton.setText(_translate("MainWindow", "Dosya Ac"))
+        self.satici_minPriceUpdate.setText(_translate("MainWindow", "Minimum Fiyat Guncelle"))
+        self.satici_minPriceUpdateButton.setText(_translate("MainWindow", "Guncelle"))
+        self.satici_restAddressUpdate.setText(_translate("MainWindow", "Restoran Adresi Guncelle"))
+        self.satici_restAddressUpdateButton.setText(_translate("MainWindow", "Guncelle"))
+        self.satici_restNameUpdate.setText(_translate("MainWindow", "Restoran ismi Guncelle"))
+        self.satici_restNameUpdateButton.setText(_translate("MainWindow", "Guncelle"))
+        
+
+
+    def importInput(self):
+        imagepath = ''
+        fileName = QtWidgets.QFileDialog.getOpenFileName(
+            caption='Open File')
+        if fileName[0] is not None and fileName[0] != "":
+            imagepath = fileName[0]
+        imagepathList = imagepath.split('/')
+        imagepath = imagepathList[-1]
+        imagepath = "assets/" + imagepath
+        self.importImagePath = imagepath
+
+    def listeyeEkle (self):
+        global cur ,conn, authInfo
+
+        productName = self.satici_productNameText.toPlainText()
+        productPrice = self.satici_productPriceText.toPlainText()
+        category = self.satici_categoryText.toPlainText()
+        ingredients = self.satici_ingredientsText.toPlainText()
+        restId = authInfo['id']
+        
+        cur.execute(
+            "INSERT INTO product(id,name,price,r_id,category,ingredients)" +
+            f"VALUES(NULL,'{productName}','{productPrice}','{restId}','{category}','{ingredients}')"
+        )
+        conn.commit()
+        print(self.importImagePath)
+        if self.importImagePath != "NULL":
+            cur.execute('SELECT id FROM product WHERE name =\"'+str(productName)+'\" and price ='+str(productPrice)+' and r_id = '+str(restId)+' and category = \"'+str(category)+'\" and ingredients = \"'+str(ingredients)+'\"' )
+            result = cur.fetchall()
+            cur.execute(
+                "INSERT INTO product_image(id,url,p_id)"+
+                f"VALUES(NULL,'{self.importImagePath}','{result[0][0]}')"
+            )
+            conn.commit()
+        self.importImagePath = "NULL"
+
+
+    def minFiyatGuncelleme(self):
+        global cur, conn, authInfo
+        restId = authInfo['id']
+        minfiyat = self.satici_minPriceUpdateText.toPlainText()
+
+        cur.execute('UPDATE restaurant SET min_pay = '+str(minfiyat)+' WHERE id = '+str(restId))
+        conn.commit()
+
+    def restAddressGuncelleme(self):
+        global cur, conn, authInfo
+        restId = authInfo['id']
+        address = self.satici_restAddressUpdateText.toPlainText()
+
+        cur.execute('UPDATE restaurant SET address = \"'+str(address)+'\" WHERE id = '+str(restId))
+        conn.commit()
+
+    def restIsmiGuncelleme(self):
+        global cur, conn, authInfo
+        restId = authInfo['id']
+
+        restAd = self.satici_restNameUpdateText.toPlainText()
+
+        cur.execute('UPDATE restaurant SET name = \"'+str(restAd)+'\" WHERE id = '+str(restId))
+
+        conn.commit()
+        
+        #goksel
+
+        
+        
+
+
+        
+        
 
 if __name__ == "__main__":
     import sys
